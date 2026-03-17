e69b68eba66f7d65350ad488fc9b66bfbe8f6a7b
AkshatSharma-cs <AkshatSharma.chennai@gmail.com>
2026-03-17 15:22:20 +0530

    Added BookMyStayApp base file

---
+++ BookMyStayApp.java
@@ -0,0 +1,15 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay Hotel Booking System!");
+
+        // Base setup: later we will add UC1–UC6 features here
+        // UC1: Room Inventory
+        // UC2: FIFO Booking Requests
+        // UC3: Double Booking Prevention
+        // UC4: Real-Time Updates
+        // UC5: Cancellation Handling
+        // UC6: Extensible Design
+    }
+}
+
